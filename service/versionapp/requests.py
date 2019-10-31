bf9ea1d53ab7d83ed3eb226a8a92bd6325b162ff
micreeson <micreeson@gmail.com>
2019-10-31 15:04:17 +0800

    发布版本时添加版本日期

--- service/versionapp/requests.py
+++ service/versionapp/requests.py
@@ -7,9 +7,10 @@ def request_set_version(request):
     app_id = request.GET.get("appid")
     version = request.GET.get("version")
     release_note = request.GET.get("note")
+    date = request.GET.get("date")
 
     version_obj = Version()
-    version_obj.set_version(app_id, version, release_note)
+    version_obj.set_version(app_id, version, release_note, date)
     json_data = json.dumps(version_obj.retrun_obj.__dict__)
     return HttpResponse(json_data, content_type="application/json")
 
--- service/versionapp/version.py
+++ service/versionapp/version.py
@@ -5,72 +5,104 @@ class ReturnObject:
     version = ''
     releaseNote = ''
     message = ''
+    releaseDate = ''
 
     def clear(self):
         self.resultCode = ''
         self.version = ''
         self.releaseNote = ''
         self.message = ''
+        self.releaseDate = ''
+
+
 class Version:
     retrun_obj = ReturnObject()
     redis_obj = RedisClient()
 
-    def set_version(self, appid, version, note):
+    def set_version(self, appid, version, note, date):
         self.retrun_obj.clear()
-        #记录当前app的最新版本号
-        key = self.gen_key_by_appid(appid)
-        self.redis_obj.write(key, version)
-        #记录当前app的版本号的发布说明
-        key = self.gen_key_by_appid_and_version(appid, version)
-        self.redis_obj.write(key, note)
+        self.save_app_version(appid, version)
+        self.save_app_version_release_note(appid, note, version)
+        self.save_app_version_date(appid, date, version)
 
         self.retrun_obj.resultCode = "OK"
         self.retrun_obj.version = self.get_latest_version(appid)
         self.retrun_obj.releaseNote = self.get_latest_note(appid)
 
+    def save_app_version_release_note(self, appid, note, version):
+        # 记录当前app的版本号的发布说明
+        key = self.gen_release_note_key(appid, version)
+        self.redis_obj.write(key, note)
+
+
+    def save_app_version_date(self, appid, date, version):
+        # 记录当前app的版本号的发布说明
+        key = self.gen_release_date_key(appid, version)
+        self.redis_obj.write(key, date)
+
+    def save_app_version(self, appid, version):
+        # 记录当前app的最新版本号
+        key = self.gen_version_key(appid)
+        self.redis_obj.write(key, version)
+
     #设置某个app的用户版本信息
-    def set_user_version(self, appid, userid):
-        key = self.gen_key_by_appid_and_userid(appid, userid)
+    def save_user_version(self, appid, userid):
+        key = self.gen_user_version_key(appid, userid)
         current_ver = self.get_latest_version(appid)
         self.redis_obj.write(key, current_ver)
 
     #获取某个app的用户的版本信息
     def get_user_version(self, appid, userid):
-        key = self.gen_key_by_appid_and_userid(appid, userid)
+        key = self.gen_user_version_key(appid, userid)
         version = self.redis_obj.read(key)
 
         return version
 
     #根据appid和userid生成key，用来存放当前用户的版本号
-    def gen_key_by_appid_and_userid(self, appid, userid):
+    def gen_user_version_key(self, appid, userid):
         if appid is None or userid is None:
             return None
 
-        return appid + '#' + userid
+        return 'VERSIONCONTROL#' + appid + '#' + userid
 
     # 根据appid和version生成key，用来存放app的版本的发布信息
-    def gen_key_by_appid_and_version(self, appid, userid):
+    def gen_release_note_key(self, appid, userid):
         if appid is None or userid is None:
             return None
 
-        return appid + '#' + userid
+        return 'VERSIONCONTROL#' + appid + '#' + userid + '#NOTE'
 
     # 根据appid生成key，用来存放app的版本号
-    def gen_key_by_appid(self, appid):
+    def gen_version_key(self, appid):
         if appid is None:
             return None
 
         return 'VERSIONCONTROL#' + appid
 
+    #根据appid生成发布日期的保存key
+    def gen_release_date_key(self, appid, version):
+        if appid is None or version is None:
+            return None
+
+        return 'VERSIONCONTROL#' + appid + '#' + version + '#DATE'
+
     #获取app的最新版本号
     def get_latest_version(self, appid):
-        key = self.gen_key_by_appid(appid)
+        key = self.gen_version_key(appid)
         return self.redis_obj.read(key)
 
     #获取app最新版本的发布说明
     def get_latest_note(self,appid):
         version = self.get_latest_version(appid)
-        key = self.gen_key_by_appid_and_version(appid, version)
+        key = self.gen_release_note_key(appid, version)
+        note = self.redis_obj.read(key)
+
+        return note
+
+    #获取app最新版本的发布日期
+    def get_latest_date(self, appid):
+        version = self.get_latest_version(appid)
+        key = self.gen_release_date_key(appid, version)
         note = self.redis_obj.read(key)
 
         return note
@@ -89,11 +121,12 @@ class Version:
         #如果已经获取过版本信息，但与app最新的版本号不一致，也需要返回新版本的版本号和发布说明
         user_version = self.get_user_version(appid, userid)
         if user_version is None or user_version != latest_version:
-            self.set_user_version(appid, userid)
-            self.retrun_obj.resultCode = "Failed"
+            self.save_user_version(appid, userid)
+            self.retrun_obj.resultCode = "False"
             self.retrun_obj.message = "Get new version"
             self.retrun_obj.version = self.get_latest_version(appid)
             self.retrun_obj.releaseNote = self.get_latest_note(appid)
+            self.retrun_obj.releaseDate = self.get_latest_date(appid)
         else:
             self.retrun_obj.resultCode = "OK"
             self.retrun_obj.message = "Latest version"
